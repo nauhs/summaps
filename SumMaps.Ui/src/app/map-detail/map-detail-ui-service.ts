9e4e05c14fea57ca5a27547461d2e21be27575f1
Shuan Rang <shuan.rang@tmp.com>
2017-04-06 07:50:33 -0400

    WIP

--- SumMaps.Ui/src/app/map-detail/map-detail-ui-service.ts
+++ SumMaps.Ui/src/app/map-detail/map-detail-ui-service.ts
@@ -100,14 +100,12 @@ export class MapDetailUiService {
         }
         else {
             if (!parentNode.children || parentNode.children.length === 0) {
-                slopeFromParent = this.calculateSlope(parentNode);
-                let amp = 120 / Math.sqrt((slopeFromParent.x * slopeFromParent.x) + (slopeFromParent.y) * (slopeFromParent.y));
-                let x = parentNode.offset.left + (amp * slopeFromParent.x);
-                let y = parentNode.offset.top + (amp * slopeFromParent.y);
-                return { left: x, top: y };
+                slopeFromParent = this.getSlopeFromParent(parentNode);
+                let deltaCoords = this.getCoordsFromSlopeAndAmplitude(slopeFromParent, 120);
+                return { left: parentNode.offset.left + deltaCoords.x, top: parentNode.offset.top + deltaCoords.y };
             }
             else {
-                let slopeFromParent = this.calculateSlope(parentNode);
+                let slopeFromParent = this.getSlopeFromParent(parentNode);
                 let baseAngle = Math.atan2(slopeFromParent.y, slopeFromParent.x);
                 let newAngle = this.getRelativeAngleForNewNode(parentNode, baseAngle);
                 let position = this.getNodePosition(parentNode, newAngle, baseAngle);
@@ -118,37 +116,148 @@ export class MapDetailUiService {
         
     }
 
-    calculateSlope(node: Node) : Point {
+    getCoordsFromSlopeAndAmplitude(slope: Point, amplitude: number): Point {
+        let amp = amplitude / Math.sqrt((slope.x * slope.x) + (slope.y) * (slope.y));
+        let x = (amp * slope.x);
+        let y = (amp * slope.y);
+        return { x: x, y: y };
+    }
+
+    getSlopeFromAngle(angle: number): Point {
+
+        let tan = Math.abs(Math.tan(angle));
+        let quadrant = this.getQuadrant(angle);
+        
+
+        // note: slope is adjusted for SVC coordinate system where (0,0) is upper left corner
+        // NOT calculated off of cartesian coordinate system where (0,0) is middle
+        // thus y value is multiplied by -1
+        //if (quadrant == 1) 
+        //    return { x: 1, y: -aTan };
+        //else if (quadrant == 2) 
+        //    return { x: -1, y: -aTan };
+        //else if (quadrant == 3)
+        //    return { x: -1, y: aTan };
+        //else (quadrant == 4)
+        //    return { x: 1, y: aTan };
+        return { x: 1, y: tan };
+    }
+
+    getSlopeFromParent(node: Node) : Point {
         let parentMid = this.getNodeMidpoint(node.parent);
         let childMid = this.getNodeMidpoint(node);
         return { x: childMid.x - parentMid.x, y: childMid.y - parentMid.y };
     }
 
+    getQuadrant(angle: Number): Number {
+        if ((-2 * Math.PI) <= angle && angle < (-1.5 * Math.PI))
+            return 4;
+        else if ((-1.5 * Math.PI) <= angle && angle < (-1 * Math.PI))
+            return 3;
+        else if ((-1 * Math.PI) <= angle && angle < (-0.5 * Math.PI))
+            return 2;
+        else if ((-.5 * Math.PI) <= angle && angle <= 0)
+            return 1;
+        else if (0 < angle && angle <= (0.5 * Math.PI))
+            return 4;
+        else if (((0.5 * Math.PI) < angle) && angle <= Math.PI)
+            return 3;
+        else if ((Math.PI < angle) && angle <= (1.5 * Math.PI))
+            return 2;
+        else
+            return 1;
+    }
+
     /// we need the relative angle, not the absolute angle
     getNodePosition(parentNode: Node, relativeAngle: number, baseAngle: number): Point {
         
-        let tan = Math.tan(relativeAngle);
-        console.log('angle: ' + relativeAngle + ' tan: ' + tan);
-        let parentMid = this.getNodeMidpoint(parentNode);
-        let amp = 150;
+        // we need to the points on the new line defined by relative angle that will be 150px
+        // away from the parent node.  this is essentially finding the intersections of a circle and a line
         
-        let xMultiplier = 1;
-        //if (((angle > .5 * Math.PI) && (angle < 1.5 * Math.PI)) || ((angle > -1.5 * Math.PI) && (angle < -.5 * Math.PI)))
-        //    xMultiplier = -1;
 
-        let yMultiplier = 1;
-        //if ((angle < 0) || angle > Math.PI)
-        //    yMultiplier = -1;
-        
-        let y = Math.sqrt((tan * tan * amp * amp) / ((tan * tan) + 1));
-        let x = Math.sqrt((amp * amp) / ((tan * tan) + 1));
+        // first coefficients for the equation of the line: 
+        // y = mx + b
+        let m = Math.tan(baseAngle + relativeAngle);  // todo, sometimes subtract
 
-        if(
-        
-        return {
-            x: parentNode.offset.left + (xMultiplier * x),
-            y: parentNode.offset.top + (yMultiplier * y)
-        };
+        // if m is essentially a straight line we can do simpler stuff
+        if (m > 1000)
+            return { x: parentNode.offset.left, y: parentNode.offset.top - 150 };
+
+        if (Math.abs(m) < 1 / 1000)
+            return { x: parentNode.offset.left - 150, y: parentNode.offset.top};
+
+        let b = parentNode.offset.top - (m * parentNode.offset.left);
+        console.log('total angle: ' + (baseAngle + relativeAngle) + ' m: ' + m + ' b: ' + b);
+
+        // using trig concepts outlined here:
+        // http://math.stackexchange.com/questions/311921/get-location-of-vector-circle-intersection
+        //
+        // the equation for our circle is (x-j)^2 + (y-k)^2 = 150^2 where (j,k) is the coordinate for the parent node
+        // to solve for x, replace y with mx + b in the circle equation
+        // we get a quadratic equation:
+        // (m^2 + 1)x^2 + (2m(b-k) - 2j)x + (b-k)^2 + j^2 - 150^2 = 0
+        //
+        // this can be written as: dx^2 + ex + f = 0
+        // and then we can apply a formula to solve for x
+        //
+        let j = parentNode.offset.left;
+        let k = parentNode.offset.top;
+
+        let d = (m * m) + 1;
+        let e = (2 * m * (b - k)) - (2 * j);
+        let f = (b - k) * (b - k) + (j * j) - (150 * 150);
+
+        // solve for x
+        // x = (-e +- Math.sqrt(e^2 - 4df)) / 2d
+        let x = (-e + Math.sqrt((e * e) - (4 * d * f))) / (2 * d);
+
+        // solve for y
+        // y = mx + b
+        let y = (m * x) + b;
+
+        console.log(x + ',' + y);
+        return { x: x, y: y };
+
+
+
+
+
+
+
+        //console.log('base: ' + baseAngle + ' rel: ' + relativeAngle);
+        //let amp = 150;
+
+        //// get coordinates for base node
+        //let slope = this.getSlopeFromAngle(baseAngle);
+        //console.log('baseSlope, deltaCoords');
+        //console.log(slope);
+        //let deltaCoords = this.getCoordsFromSlopeAndAmplitude(slope, amp);
+        //console.log(deltaCoords);
+        //let baseNode = { x: parentNode.offset.left + deltaCoords.x, y: parentNode.offset.top + deltaCoords.y };
+
+        //// get delta coords for relative angle
+        //let relativeSlope = this.getSlopeFromAngle(relativeAngle);
+        //let relativeCoords = this.getCoordsFromSlopeAndAmplitude(relativeSlope, amp);
+        //console.log('relSlope, relativeCoords');
+        //console.log(relativeSlope);
+        //console.log(relativeCoords);
+
+        //let quadrant = this.getQuadrant(relativeAngle);
+        //let coords: Point;
+        //if (quadrant == 1) 
+        //    coords = { x: baseNode.x - relativeCoords.x, y: baseNode.y - relativeCoords.y };
+        //else if (quadrant == 2)
+        //    coords = { x: parentNode.offset.left - relativeCoords.x, y: baseNode.y - relativeCoords.y };
+        //else if (quadrant == 3)
+        //    coords = { x: parentNode.offset.left - relativeCoords.x, y: baseNode.y + relativeCoords.y };
+        //else 
+        //    coords = { x: baseNode.x - relativeCoords.x, y: baseNode.y + relativeCoords.y };
+
+        //console.log('angle: ' + relativeAngle + 'quadrant: ' + quadrant + ' coords:' );
+        //console.log(coords);
+
+        //return coords;
+
     }
 
     getRelativeAngleForNewNode(node: Node, baseAngle: number): number {
@@ -159,24 +268,25 @@ export class MapDetailUiService {
 
         for (let c of node.children) {
             // get angles relative to the base line which is the line from this node to its parent
-            let angle = this.calculateAngleDiff(baseAngle, this.calculateSlope(c));
-            console.log('angle: ' + angle);
+            let angle = this.calculateAngleDiff(baseAngle, this.getSlopeFromParent(c));
             angles.push(angle);
         }
 
-        let sortedAngles = angles.sort((a1, a2) => (a1 > a2) ? 1 : ((a1 == a2) ? 0 : -1));
-
-        let a1: number, a2: number, maxDiff: number = 0;
 
         // if this is a root node, create child nodes at any angle
         // if this is a child node, don't create child nodes at angles greater than 90 degrees
         let boundaryAngle = (!node.parent ? Math.PI : Math.PI / 2);
-
         angles.push(boundaryAngle);
         angles.push(-boundaryAngle);
         
+        let sortedAngles = angles.sort((a1, a2) => (a1 > a2) ? 1 : ((a1 == a2) ? 0 : -1));
+
+        let a1: number, a2: number, maxDiff: number = 0;
+        
         for (let i = 1; i < sortedAngles.length; i++) {
-            
+
+            console.log('angle: ' + sortedAngles[i - 1]);
+
             if (sortedAngles[i] > boundaryAngle || sortedAngles[i] < -boundaryAngle)
                 continue;
 
@@ -188,11 +298,9 @@ export class MapDetailUiService {
                 maxDiff = diff;
             }
         }
-
-        //console.log('a1:' + a1 + ' a2:' + a2 + ' diff:' + (a2 - a1) / 2) + ' vacant angle: ' + a1 + ((a2 - a1) / 2));
-        console.log('a1:' + a1 + ' a2:' + a2);
-        //return this.calculateAbsoluteAngle(Math.atan2(slopeFromParent.y, slopeFromParent.x), a1 + ((a2 - a1) / 2));
-        return a1 + ((a2 - a1) / 2);
+        
+        console.log('a1:' + a1 + ' a2:' + a2 + 'new: ' + ((a1 + ((a2 - a1) / 2))));
+        return (a1 + ((a2 - a1) / 2));
     }
 
     calculateAbsoluteAngle(baselineAngle: number, relativeAngle: number) {
--- SumMaps.Ui/src/app/map-detail/node.service.ts
+++ SumMaps.Ui/src/app/map-detail/node.service.ts
@@ -28,24 +28,36 @@ export class NodeService {
         if (this.nodes)
             return Promise.resolve(this.nodes);
 
-        var rn = this.createNode('rn', { top: 200, left: 400 }, 'RN', 0, 0);
-        var cn1 = this.createNode('cn1', { top: 250, left: 600 }, 'CN1', 1, 1);
-        var gn1_1 = this.createNode('gn', { top: 300, left: 800 }, 'GN1.1', 1, 2);
+        //var rn = this.createNode('rn', { top: 200, left: 400 }, 'RN', 0, 0);
+        //var cn1 = this.createNode('cn1', { top: 250, left: 600 }, 'CN1', 1, 1);
+        //var gn1_1 = this.createNode('gn', { top: 300, left: 800 }, 'GN1.1', 1, 2);
 
-        var cn2 = this.createNode('cn2', { top: 250, left: 600 }, 'CN2', 2, 1);
-        var cn3 = this.createNode('cn3', { top: 250, left: 600 }, 'CN3', 3, 1);
-        var cn4 = this.createNode('cn4', { top: 250, left: 600 }, 'CN4', 4, 1);
+        //var cn2 = this.createNode('cn2', { top: 250, left: 600 }, 'CN2', 2, 1);
+        //var cn3 = this.createNode('cn3', { top: 250, left: 600 }, 'CN3', 3, 1);
+        //var cn4 = this.createNode('cn4', { top: 250, left: 600 }, 'CN4', 4, 1);
 
-        rn.children = [cn1, cn2, cn3, cn4];
+        //rn.children = [cn1, cn2, cn3, cn4];
+        //cn1.parent = rn;
+        //cn1.children = [gn1_1];
+        //gn1_1.parent = cn1;
+
+        //cn2.parent = rn;
+        //cn3.parent = rn;
+        //cn4.parent = rn;
+
+        //this.nodes = [rn, cn1, cn2, cn3, cn4, gn1_1];
+
+        var rn = this.createNode('rn', { top: 200, left: 400 }, 'x:400, y:200', 0, 0);
+        var cn1 = this.createNode('cn1', { top: 200, left: 600 }, 'x:600 y:200', 1, 1);
+
+        var cn2 = this.createNode('cn2', { top: 100, left: 400 }, 'x:400, y:100', 2, 1);
+
+        rn.children = [cn1, cn2];
         cn1.parent = rn;
-        cn1.children = [gn1_1];
-        gn1_1.parent = cn1;
 
         cn2.parent = rn;
-        cn3.parent = rn;
-        cn4.parent = rn;
 
-        this.nodes = [rn, cn1, cn2, cn3, cn4, gn1_1];
+        this.nodes = [rn, cn1, cn2];
         return Promise.resolve(this.nodes);
     }
 
@@ -68,7 +80,7 @@ export class NodeService {
         var branchDepth = node.branchDepth + 1;
 
         let offset = this.mapDetailUiService.getNewNodeOffset(node);
-        let newNode = this.createNode(id, offset, id, branch, branchDepth); 
+        let newNode = this.createNode(id, offset, 'x ' + offset.left + ' y:' + offset.top, branch, branchDepth); 
         newNode.parent = node;
         if (!node.children)
             node.children = [];
